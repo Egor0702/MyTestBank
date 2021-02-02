37377e731e4d0e6a3404c50735d349aacefa7b2c
Egor0702 <65344158+Egor0702@users.noreply.github.com>
2021-02-02 17:52:45 +0300

    Update EndActivity.kt
    Add two methods: getNumbersOfUnicornGroups() and getNameOfTheUnicorn(). They process data from another activity and determinate the result.

--- app/src/main/java/marynych/rsue/mytestbank/EndActivity.kt
+++ app/src/main/java/marynych/rsue/mytestbank/EndActivity.kt
@@ -4,8 +4,44 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class EndActivity : AppCompatActivity() {
+    lateinit var resultOfTheText : TextView
+    lateinit var returnButton : Button
+    lateinit var imageOfUnicorn : ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_end)
+        
+        //здесь проинициализируем переменные тестового поля, кнопки и изображения
+        
+        
     }
+    fun getNumbersOfUnicornGroups(){
+        var firstTypeOfUnicorn : Int = 0
+        var secondTypeOfUnicorn : Int = 0
+        var thirdTypeOfUnicorn : Int = 0
+        val numbersGroup : Array<Int> = getIntent().getIntArrayExtra("array")
+        for (i : Int in.. numbersGroup){
+        when(i){
+        1 -> firstTypeOfUnicorn++
+        2 -> secondTypeOfUnicorn++
+        3 -> thirdTypeOfUnicorn ++
+        }
+        }
+        getNameOfTheUnicorn(firstTypeOfUnicorn, secondTypeOfUnicorn, thirdTypeOfUnicorn)
+    }
+        fun getNameOfTheUnicorn(oneType : Int, twoType : Int, threeType : Int) : String{
+        if (oneType > twoType && oneType > threeType)
+        return "Вы красный единорог!"
+        if (twoType > oneType && twoType > threeType)
+        return "Вы желтый единорог!"
+        if(threeType > oneType && threeType > twoType)
+        return "Вы розовый единорог!"
+         if(oneType == twoType)
+            return "Вы красный единорог!"
+            if (oneType == threeType)
+            return "Вы розовый единорог!"
+            else
+            return "Вы желтый единорог!"
+    }
+    
 }
