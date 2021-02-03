08a0a668a2a0667143e67452b467b0114ea45ef0
Egor <65344158+Egor0702@users.noreply.github.com>
2021-02-03 17:10:30 +0300

    Update EndActivity.kt
    Added new method setResultTest(), changed variables name and imageRes in method getNameOfTheUnicorn(). Initialized variables: resultOfTheText, returnButton, imageOfUnicorn.

--- app/src/main/java/marynych/rsue/mytestbank/EndActivity.kt
+++ app/src/main/java/marynych/rsue/mytestbank/EndActivity.kt
@@ -12,8 +12,14 @@ class EndActivity : AppCompatActivity() {
         setContentView(R.layout.activity_end)
         
         //здесь проинициализируем переменные тестового поля, кнопки и изображения
+        resultOfTheText = findViewById(R.id.textView)
+        returnButton = findViewById(R.id.button_return)
+        imageOfUnicorn = findViewById(R.id.imageView2)
         
-        
+        returnButton.setOnClickListener{
+        val intent = Intent (this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
     fun getNumbersOfUnicornGroups(){
         var firstTypeOfUnicorn : Int = 0
@@ -29,19 +35,39 @@ class EndActivity : AppCompatActivity() {
         }
         getNameOfTheUnicorn(firstTypeOfUnicorn, secondTypeOfUnicorn, thirdTypeOfUnicorn)
     }
-        fun getNameOfTheUnicorn(oneType : Int, twoType : Int, threeType : Int) : String{
-        if (oneType > twoType && oneType > threeType)
-        return "Вы красный единорог!"
-        if (twoType > oneType && twoType > threeType)
-        return "Вы желтый единорог!"
-        if(threeType > oneType && threeType > twoType)
-        return "Вы розовый единорог!"
-         if(oneType == twoType)
-            return "Вы красный единорог!"
-            if (oneType == threeType)
-            return "Вы розовый единорог!"
-            else
-            return "Вы желтый единорог!"
+        fun getNameOfTheUnicorn(oneType : Int, twoType : Int, threeType : Int) : Int, Int{
+        val name : Int = 0
+        val imageRes : Int = 0
+        if (oneType > twoType && oneType > threeType){
+        name = R.id.type_of_unicorn_one
+        imageRes = R.drawable.red_unicorn
+        }
+        if (twoType > oneType && twoType > threeType){
+        name = R.id.type_of_unicorn_two
+        imageRes = R.drawable.yellow_unicorn_1
+        }
+        if(threeType > oneType && threeType > twoType){
+        name = R.id.type_of_unicorn_three
+        imageRes = R.drawable.pink_unicorn
+        }
+         if(oneType == twoType){
+        name = R.id.type_of_unicorn_one
+        imageRes = R.drawable.red_unicorn
+        }
+            if (oneType == threeType){
+        name = R.id.type_of_unicorn_three
+        imageRes = R.drawable.pink_unicorn
+        }
+            else{
+        name = R.id.type_of_unicorn_two
+        imageRes = R.drawable.yellow_unicorn_1
+        }
+        
+            setResultTest(name, imageRes)
+    }
+    fun setResultTest(name, imageRes){
+    resultOfTheText.setText(name)
+    imageOfUnicorn.setImageResource(imageRes)
     }
     
 }
