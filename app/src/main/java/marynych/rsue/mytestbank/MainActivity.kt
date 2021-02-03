cbd72209ea135b33d4c35d4be44aa42b31922dba
Egor <65344158+Egor0702@users.noreply.github.com>
2021-02-03 17:00:42 +0300

    Update MainActivity.kt
    we have written a code for saving the state of variables when the activity state changes. And delete method testResult().

--- app/src/main/java/marynych/rsue/mytestbank/MainActivity.kt
+++ app/src/main/java/marynych/rsue/mytestbank/MainActivity.kt
@@ -13,13 +13,18 @@ class MainActivity : AppCompatActivity() {
     lateinit var variantTwo: CheckBox
     lateinit var variantThree: CheckBox
 
-    var arrayAnswer:Arr
+    var arrayAnswer:MutableList<Int>() = mutableListOf()
     var mCurrIndex = 0
     val mBankQuestionCorn : List<Question> = listOf(
         Question("Какой твой любимый цвет?", "Зеленый", 1,  "Розовый", 3,"Желтый", 2),
         Question("Что лучше всего описывает тебя:", "Дружелюбный, открыт к общению", 3, "Люблю природу, тишину", 1, "Разве есть кто-то лучше меня?!", 2)
     )
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?)
+    if (savedInstanceState != null){
+    mCurrIndex = savedInstanceState.getInt("currentNumber", 0)
+    arrayAnswer = savedInstanceState.getIntArrayExtra("array", null)
+    }
+    
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -40,29 +45,53 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, R.string.stop_name, Toast.LENGTH_SHORT).show()
         }
         variantOne.setOnCheckedChangeListener{ compoundButton: CompoundButton, b: Boolean ->
-          val text: String = variantOne.text.toString()
-            if (variantOne.isChecked())
-              mapAnswer.put(mCurrIndex, 1)
-            else
-              mapAnswer.remove(mCurrIndex)
-
+           val textVariantOne: String = variantOne.text.toString()
+          var answerVariantOne: Int = 0
+            if (variantOne.isChecked()){
+              if(mBankQuestionCorn[mCurrIndex].answerOne == textVariantOne)
+                answerVariantOne = (mBankQuestionCorn[mCurrIndex].keyOne
+              if (mBankQuestionCorn[mCurrIndex].answerTwo == textVariantOne)
+                 answerVariantOne = (mBankQuestionCorn[mCurrIndex].keyTwo
+               if (mBankQuestionCorn[mCurrIndex].answerThree == textVariantOne)
+                 answerVariantOne = (mBankQuestionCorn[mCurrIndex].keyThree   
+                           arrayAnswer.add( answerVariantOne)
+            }else
+                 arrayAnser.remove( answerVariantOne)                    
         }
         variantTwo.setOnCheckedChangeListener{buttonView: CompoundButton, isChecked: Boolean ->
-            if (variantTwo.isChecked())
-                mapAnswer.put(mCurrIndex, 2)
-            else
-                mapAnswer.remove(mCurrIndex)
+            val textVariantTwo: String = variantTwo.text.toString()
+          var answerVariantTwo: Int = 0
+            if (variantOne.isChecked()){
+              if(mBankQuestionCorn[mCurrIndex].answerOne == textVariantTwo)
+                answerVariantTwo = (mBankQuestionCorn[mCurrIndex].keyOne
+              if (mBankQuestionCorn[mCurrIndex].answerTwo == textVariantTwo)
+                 answerVariantTwo = (mBankQuestionCorn[mCurrIndex].keyTwo
+               if (mBankQuestionCorn[mCurrIndex].answerThree == textVariantTwo)
+                 answerVariantTwo = (mBankQuestionCorn[mCurrIndex].keyThree   
+              arrayAnswer.add(answerVariantTwo)
+            }else
+                 arrayAnser.remove( answerVariantTwo)   
         }
         variantThree.setOnCheckedChangeListener{buttonView: CompoundButton, isChecked: Boolean ->
-            if (variantThree.isChecked())
-                mapAnswer.put(mCurrIndex, 3)
-            else
-                mapAnswer.remove(mCurrIndex)
+            val textVariantThree: String = variantThree.text.toString()
+          var answerVariantThree: Int = 0
+            if (variantOne.isChecked()){
+              if(mBankQuestionCorn[mCurrIndex].answerOne == textVariantTwo)
+                answerVariantTwo = (mBankQuestionCorn[mCurrIndex].keyOne
+              if (mBankQuestionCorn[mCurrIndex].answerTwo == textVariantTwo)
+                 answerVariantTwo = (mBankQuestionCorn[mCurrIndex].keyTwo
+               if (mBankQuestionCorn[mCurrIndex].answerThree == textVariantTwo)
+                 answerVariantTwo = (mBankQuestionCorn[mCurrIndex].keyThree   
+              arrayAnswer.add(answerVariantThree)
+            }else
+                 arrayAnser.remove( answerVariantThree)   
+        }
         }
         mNextButton.setOnClickListener {
            if(mNextButton.text == "Завершить опрос"){
                val intent = Intent(this, EndActivity::class.java)
-               intent.putExtra("key", mapAnswer)
+               arr : Array<Int> = arrayAnswer.toArray()
+               intent.putExtra("array", arr) //putIntArrayExtra ?
                startActivity(intent)
            }
             var nextIndex = mCurrIndex
@@ -74,6 +103,14 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, R.string.stop_name, Toast.LENGTH_SHORT).show()
         }
     }
+    override fun onSaveInstanceState(Bundle outState){
+    super.onSaveInstanceState(outState)
+        outState.putInt("currentNumber", mCurrIndex)
+        arr : Array<Int> = arrayAnswer.toArray()
+        outState.putIntArrayExtra("array", arr)
+        
+    }
+
         fun setQuestion(index:Int){
             if(index == mBankQuestionCorn.size - 1)
                 mNextButton.setText(R.string.button_name_two)
