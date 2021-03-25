f7e085d76ec32abbfa3a807abbfc3da51f524ec7
Egor0702 <marynych.egor@mail.ru>
2021-03-25 10:33:21 +0300

    Made changes in activity_end, manifest and EndActivity.kt

--- app/src/main/java/marynych/rsue/mytestbank/EndActivity.kt
+++ app/src/main/java/marynych/rsue/mytestbank/EndActivity.kt
@@ -1,6 +1,7 @@
 package marynych.rsue.mytestbank
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -9,15 +10,16 @@ import android.widget.TextView
 
 class EndActivity : AppCompatActivity() {
     lateinit var resultOfTheText : TextView
-
+    lateinit var textView: TextView
     lateinit var imageOfUnicorn : ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_end)
         
         //здесь проинициализируем переменные тестового поля, кнопки и изображения
-        resultOfTheText = findViewById(R.id.textView)
+        resultOfTheText = findViewById(R.id.resultOfTheText)
         imageOfUnicorn = findViewById(R.id.imageBackGround)
+        textView = findViewById(R.id.textView)
 
         getNumbersOfUnicornGroups()
     }
@@ -37,46 +39,62 @@ class EndActivity : AppCompatActivity() {
         getNameOfTheUnicorn(firstTypeOfUnicorn, secondTypeOfUnicorn, thirdTypeOfUnicorn)
     }
         fun getNameOfTheUnicorn(oneType : Int, twoType : Int, threeType : Int){
-        var name : String = ""
-        var imageRes : Int = 0
+        var name = ""
+        var colorName = 0
+        var imageRes = 0
+        var text = ""
             println(oneType > twoType && oneType > threeType)
             val i = 1
             for (i in 1..1) {
                 if (oneType > twoType && oneType > threeType) {
                     name = getString(R.string.type_of_unicorn_one)
+                    colorName = Color.RED
                     imageRes = R.drawable.red_unicorn300
+                    text = getString(R.string.red_unicorn_wiki)
                     break
                 }
                 if (twoType > oneType && twoType > threeType) {
                     name = getString(R.string.type_of_unicorn_two)
+                    colorName = Color.YELLOW
                     imageRes = R.drawable.yellow_unicorn
+                    text = getString(R.string.yellow_unicorn_wiki)
                     break
                 }
                 if (threeType > oneType && threeType > twoType) {
                     name = getString(R.string.type_of_unicorn_three)
+                    colorName = Color.parseColor("#fc0fc0")
                     imageRes = R.drawable.pink_unicorn1
+                    text = getString(R.string.pink_unicorn_wiki)
                     break
                 }
                 if (oneType == twoType) {
                     name = getString(R.string.type_of_unicorn_one)
+                    colorName = Color.RED
                     imageRes = R.drawable.red_unicorn300
+                    text = getString(R.string.red_unicorn_wiki)
                     break
                 }
                 if (oneType == threeType) {
                     name = getString(R.string.type_of_unicorn_three)
+                    colorName = Color.parseColor("#fc0fc0")
                     imageRes = R.drawable.pink_unicorn1
+                    text = getString(R.string.pink_unicorn_wiki)
                     break
                 }
                 else {
                     name = getString(R.string.type_of_unicorn_two)
+                    colorName = Color.YELLOW
                     imageRes = R.drawable.yellow_unicorn
+                    text = getString(R.string.yellow_unicorn_wiki)
                 }
             }
-            setResultTest(name, imageRes)
+            setResultTest(name, colorName, imageRes, text)
     }
-    fun setResultTest(name: String, imageRes:Int){
-    resultOfTheText.setText("Ты $name")
+    fun setResultTest(name: String, colorName : Int, imageRes:Int, text : String){
+    resultOfTheText.setText("$name")
+        resultOfTheText.setTextColor(colorName)
     imageOfUnicorn.setImageResource(imageRes)
+        textView.setText(text)
     }
 
     override fun onBackPressed() {
